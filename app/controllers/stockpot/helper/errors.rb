28dfd0bcda5d8492528c8094972c0ad38a43c6c0
Victor Santana <victor.santana@freshly.com>
2020-02-06 14:58:12 -0700

    Refactor
    Possible refactor

---
+++ app/controllers/stockpot/helper/errors.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+module Stockpot
+  module Helper
+    module Errors
+      def rescue_error(error)
+        logger = Logger.new(STDERR)
+        logger.warn(error)
+
+        case error
+        when NameError
+          return_error(error.to_s, error.backtrace.first(5), :bad_request)
+        when PG::Error
+          return_error("Postgres error: #{error}", error.backtrace.first(5), :server_error)
+        when ActiveRecord::RecordInvalid, ActiveRecord::Validations
+          return_error("ActiveRecord error: ActiveRecord error: #{error}", error.backtrace.first(5), :server_error)
+        else
+          return_error(error.to_s, error.backtrace.first(5),:server_error)
+        end
+      end
+
+      def return_error(message, backtrace, status)
+        { json: { error: { status: status, backtrace: backtrace, message: message }}, status: status }
+      end
+    end
+  end
+end
--- app/controllers/stockpot/records_controller.rb
+++ app/controllers/stockpot/records_controller.rb
@@ -1,19 +1,16 @@
 # frozen_string_literal: true
-
-require_dependency "stockpot/application_controller"
-require_relative "../../../lib/stockpot/errors"
-
 require "factory_bot_rails"
 
 module Stockpot
   class RecordsController < ApplicationController
     include ActiveSupport::Inflector
+    include ActiveRecord::Transactions
 
     before_action only: %i[index destroy update] do
-      Errors.return_error("You need to provide at least one model name as an argument", 400) if params.dig(:models).blank?
+      return_error("You need to provide at least one model name as an argument", 400) if params.dig(:models).blank?
     end
     before_action only: %i[create] do
-      Errors.return_error("You need to provide at least one factory name as an argument", 400) if params.dig(:factory).blank?
+      return_error("You need to provide at least one factory name as an argument", 400) if params.dig(:factory).blank?
     end
 
     def index
@@ -25,46 +22,43 @@ module Stockpot
       end
 
       render json: obj, status: :ok
-    rescue StandardError => exception
-      render Errors.rescue_error(exception)
     end
 
     def create
       list = params[:list] || 1
-      list.times do |n|
-        all_parameters = [ factory, *traits, attributes(n) ].compact
-        FactoryBot.create(*all_parameters)
+      ActiveRecord::Base.transaction do
+        list.times do |n|
+          all_parameters = [ factory, *traits, attributes(n) ].compact
+          FactoryBot.create(*all_parameters)
+        end
       end
       obj = factory.to_s.camelize.constantize.last(list)
 
       render json: obj, status: :created
-    rescue StandardError => exception
-      render Errors.rescue_error(exception)
     end
 
     def destroy
       obj = {}
-      models.each_with_index do |element, i|
-        model = element[:model].to_s
-        obj[pluralize(model).camelize(:lower)] = model.camelize.constantize.where(models[i].except(:model)).destroy_all
+      ActiveRecord::Base.transaction do
+        models.each_with_index do |element, i|
+          model = element[:model].to_s
+          obj[pluralize(model).camelize(:lower)] = model.camelize.constantize.where(models[i].except(:model)).destroy_all
+        end
       end
 
       render json: obj, status: :accepted
-    rescue StandardError => exception
-      render Errors.rescue_error(exception)
     end
 
     def update
       obj = {}
-      models.each_with_index do |element, i|
-        model = element[:model].to_s
-        update_params = params.permit![:models][i][:update].to_h
-        obj[pluralize(model).camelize(:lower)] = model.camelize.constantize.where(models[i].except(:model, :update)).update(update_params)
+      ActiveRecord::Base.transaction do
+        models.each_with_index do |element, i|
+          model = element[:model].to_s
+          update_params = params.permit![:models][i][:update].to_h
+          obj[pluralize(model).camelize(:lower)] = model.camelize.constantize.where(models[i].except(:model, :update)).update(update_params)
+        end
       end
-
       render json: obj, status: :accepted
-    rescue StandardError => exception
-      render Errors.rescue_error(exception)
     end
 
     private

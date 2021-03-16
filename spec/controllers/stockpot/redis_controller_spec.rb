850f40d42ceffae9456b3d9491889dd1ebbcf806
Victor Santana <victor.santana@freshly.com>
2021-03-16 15:55:32 -0700

    Small refactor

--- spec/controllers/stockpot/redis_controller_spec.rb
+++ spec/controllers/stockpot/redis_controller_spec.rb
@@ -2,31 +2,44 @@
 
 RSpec.describe Stockpot::RedisController, type: :request do
   let(:body) { { status: 200  }.to_json }
+  let(:key) { "test" }
+  let(:value) { "test_value" }
+  let(:field) { "field" }
 
   describe "GET #index" do
     it "returns the redis value" do
-      REDIS.set("test", "test_value")
-      get redis_path, params: { key: "test" }
-      expect(response.body).to eq("test_value".to_json)
+      REDIS.set(key, value)
+      get redis_path, params: { key: key }
+      expect(response.body).to eq(value.to_json)
       expect(response.status).to eq(200)
     end
     it "returns the redis value with field" do
-      REDIS.hset("myhash", "field", "test")
-      get redis_path, params: { key: "myhash", field: "field" }
-      expect(response.body).to eq("test".to_json)
+      REDIS.hset(key, field, value)
+      get redis_path, params: { key: key, field: field }
+      expect(response.body).to eq(value.to_json)
       expect(response.status).to eq(200)
     end
   end
   describe "POST #create" do
     it "creates a redis hash" do
-      post redis_path, params: { key: "test", value: "test_value" }
-      expect(REDIS.get("test")).to eq("test_value")
+      post redis_path, params: { key: key, value: value }
+      expect(REDIS.get(key)).to eq(value)
       expect(response.status).to eq(200)
     end
     it "creates a redis hash with field" do
-      post redis_path, params: { key: "test", value: "test_value", field: "field" }
-      expect(REDIS.hget("test", "field")).to eq("test_value")
+      post redis_path, params: { key: key, value: value, field: field }
+      expect(REDIS.hget(key, field)).to eq(value)
       expect(response.status).to eq(200)
+    end
+  end
+  describe "GET #keys" do
+    it "gets all keys" do
+      keys = ["test1", "test2"]
+      REDIS.set(keys[0], "test1_key")
+      REDIS.set(keys[1], "test2_key")
+
+      get redis_keys_path
+      expect(response.body).to eq(keys.to_json)
     end
   end
 end
